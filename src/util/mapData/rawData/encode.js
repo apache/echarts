badce0dfbea7592d67433b6b677833102e24d9b0
lang <bm2736892@gmail.com>
2013-12-10 19:04:34 +0800

    计算world geo中的cp，fix utf-8字符为8232时js的语法错误

--- src/util/mapData/rawData/encode.js
+++ src/util/mapData/rawData/encode.js
@@ -17,11 +17,17 @@ glob('geoJson/*.json', {}, function(err, files) {
         features.forEach(function(feature){
             var encodeOffsets = feature.geometry.encodeOffsets = [];
             var coordinates = feature.geometry.coordinates;
+            var bb = [[999999, 999999], [-999999, -999999]];
             if (feature.geometry.type === 'Polygon') {
                 coordinates.forEach(function(coordinate, idx){
                     coordinates[idx] = encodePolygon(
                         coordinate, encodeOffsets[idx] = []
                     );
+                    var _bb = computeBoundingBox(coordinate);
+                    bb[0][0] = Math.min(bb[0][0], _bb[0][0]);
+                    bb[0][1] = Math.min(bb[0][1], _bb[0][1]);
+                    bb[1][0] = Math.max(bb[1][0], _bb[1][0]);
+                    bb[1][1] = Math.max(bb[1][1], _bb[1][1]);
                 });
             } else if(feature.geometry.type === 'MultiPolygon') {
                 coordinates.forEach(function(polygon, idx1){
@@ -30,16 +36,38 @@ glob('geoJson/*.json', {}, function(err, files) {
                         coordinates[idx1][idx2] = encodePolygon(
                             coordinate, encodeOffsets[idx1][idx2] = []
                         );
+                        var _bb = computeBoundingBox(coordinate);
+                        bb[0][0] = Math.min(bb[0][0], _bb[0][0]);
+                        bb[0][1] = Math.min(bb[0][1], _bb[0][1]);
+                        bb[1][0] = Math.max(bb[1][0], _bb[1][0]);
+                        bb[1][1] = Math.max(bb[1][1], _bb[1][1]);
                     });
                 });
             }
+            if (!feature.properties.cp) {
+                feature.properties.cp = [(bb[0][0] + bb[1][0]) / 2, (bb[0][1] + bb[1][1]) / 2];
+            }
         });
-
         fs.writeFileSync(
             output, addAMDWrapper(JSON.stringify(json)), 'utf8'
         );
     });
 });
+
+function computeBoundingBox(coordinate) {
+    var min = coordinate[0].slice();
+    var max = min.slice();
+
+    for (var i = 0; i < coordinate.length; i++) {
+        var point = coordinate[i];
+        min[0] = Math.min(point[0], min[0]);
+        min[1] = Math.min(point[1], min[1]);
+        max[0] = Math.max(point[0], max[0]);
+        max[1] = Math.max(point[1], max[1]);
+    }
+
+    return [min, max];
+}
 
 function encodePolygon(coordinate, encodeOffsets) {
     var result = '';
@@ -77,7 +105,17 @@ function encode(val, prev){
     // ZigZag
     val = (val << 1) ^ (val >> 15);
     // add offset and get unicode
-    return String.fromCharCode(val+64);
+    if (val+64 === 8232) { //WTF, 8232 will get syntax error in js code
+        val--;
+    }
+    var str = String.fromCharCode(val+64);
+    // var tmp = {'tmp' : str};
+    // try{
+    //     eval("(" + JSON.stringify(tmp) + ")");
+    // }catch(e) {
+    //     console.log(val + 64);
+    // }
+    return str;
 }
 
 function quantize(val) {
