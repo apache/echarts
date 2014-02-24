507a0c0fd05805b7197e2fdb7fbc4a46bc4346f7
kener <kener.linfeng@gmail.com>
2014-02-24 19:52:44 +0800

    纯粹为了占坑用的默认主题，开发中

---
+++ src/theme/default.js
@@ -0,0 +1,22 @@
+/**
+ * echarts默认主题，开发中
+ *
+ * @desc echarts基于Canvas，纯Javascript图表库，提供直观，生动，可交互，可个性化定制的数据统计图表。
+ * @author Kener (@Kener-林峰, linzhifeng@baidu.com)
+ *
+ */
+define(function() {
+    var config = {
+        // 默认色板
+        color: [
+            '#ff9277', ' #dddd00', ' #ffc877', ' #bbe3ff', ' #d5ffbb',
+            '#bbbbff', ' #ddb000', ' #b0dd00', ' #e2bbff', ' #ffbbe3',
+            '#ff7777', ' #ff9900', ' #83dd00', ' #77e3ff', ' #778fff',
+            '#c877ff', ' #ff77ab', ' #ff6600', ' #aa8800', ' #77c7ff',
+            '#ad77ff', ' #ff77ff', ' #dd0083', ' #777700', ' #00aa00',
+            '#0088aa', ' #8400dd', ' #aa0088', ' #dd0000', ' #772e00'
+        ]
+    };
+
+    return config;
+});
