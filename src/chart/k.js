5d741959767644ea7bf722d1e40a0718777c1d8d
kener <kener.linfeng@gmail.com>
2014-04-24 15:17:47 +0800

    图表重构 k

--- src/chart/k.js
+++ src/chart/k.js
@@ -33,15 +33,17 @@ define(function (require) {
         // 可计算特性装饰
         CalculableBase.call(this);
 
-        var self = this;
-        self.type = ecConfig.CHART_TYPE_K;
-
-        var series;                 // 共享数据源，不要修改跟自己无关的项
-
-        var _zlevelBase = self.getZlevelBase();
-
-        function _buildShape() {
-            self.selectedMap = {};
+        this.init(option, component);
+    }
+    
+    K.prototype = {
+        type : ecConfig.CHART_TYPE_K,
+        /**
+         * 绘制图形
+         */
+        _buildShape : function () {
+            var series = this.series;
+            this.selectedMap = {};
 
             // 水平垂直双向series索引 ，position索引到seriesIndex
             var _position2sIndexMap = {
@@ -51,8 +53,8 @@ define(function (require) {
             var xAxis;
             for (var i = 0, l = series.length; i < l; i++) {
                 if (series[i].type == ecConfig.CHART_TYPE_K) {
-                    series[i] = self.reformOption(series[i]);
-                    xAxis = component.xAxis.getAxis(series[i].xAxisIndex);
+                    series[i] = this.reformOption(series[i]);
+                    xAxis = this.component.xAxis.getAxis(series[i].xAxisIndex);
                     if (xAxis.type == ecConfig.COMPONENT_TYPE_AXIS_CATEGORY
                     ) {
                         _position2sIndexMap[xAxis.getPosition()].push(i);
@@ -62,49 +64,51 @@ define(function (require) {
             //console.log(_position2sIndexMap)
             for (var position in _position2sIndexMap) {
                 if (_position2sIndexMap[position].length > 0) {
-                    _buildSinglePosition(
+                    this._buildSinglePosition(
                         position, _position2sIndexMap[position]
                     );
                 }
             }
 
-            for (var i = 0, l = self.shapeList.length; i < l; i++) {
-                zr.addShape(self.shapeList[i]);
+            for (var i = 0, l = this.shapeList.length; i < l; i++) {
+                this.zr.addShape(this.shapeList[i]);
             }
-        }
+        },
 
         /**
          * 构建单个方向上的K线图
          *
          * @param {number} seriesIndex 系列索引
          */
-        function _buildSinglePosition(position, seriesArray) {
-            var mapData = _mapData(seriesArray);
+        _buildSinglePosition : function (position, seriesArray) {
+            var series = this.series;
+            var mapData = this._mapData(seriesArray);
             var locationMap = mapData.locationMap;
             var maxDataLength = mapData.maxDataLength;
 
             if (maxDataLength === 0 || locationMap.length === 0) {
                 return;
             }
-            _buildHorizontal(maxDataLength, locationMap);
+            this._buildHorizontal(maxDataLength, locationMap);
 
             for (var i = 0, l = seriesArray.length; i < l; i++) {
-                self.buildMark(
+                this.buildMark(
                     series[seriesArray[i]],
                     seriesArray[i],
-                    component
+                    this.component
                 );
             }
-        }
+        },
 
         /**
          * 数据整形
          * 数组位置映射到系列索引
          */
-        function _mapData(seriesArray) {
+        _mapData : function (seriesArray) {
+            var series = this.series;
             var serie;                              // 临时映射变量
             var serieName;                          // 临时映射变量
-            var legend = component.legend;
+            var legend = this.component.legend;
             var locationMap = [];                   // 需要返回的东西：数组位置映射到系列索引
             var maxDataLength = 0;                  // 需要返回的东西：最大数据长度
             // 计算需要显示的个数和分配位置并记在下面这个结构里
@@ -112,12 +116,12 @@ define(function (require) {
                 serie = series[seriesArray[i]];
                 serieName = serie.name;
                 if (legend){
-                    self.selectedMap[serieName] = legend.isSelected(serieName);
+                    this.selectedMap[serieName] = legend.isSelected(serieName);
                 } else {
-                    self.selectedMap[serieName] = true;
+                    this.selectedMap[serieName] = true;
                 }
 
-                if (self.selectedMap[serieName]) {
+                if (this.selectedMap[serieName]) {
                     locationMap.push(seriesArray[i]);
                 }
                 // 兼职帮算一下最大长度
@@ -127,12 +131,13 @@ define(function (require) {
                 locationMap : locationMap,
                 maxDataLength : maxDataLength
             };
-        }
+        },
 
         /**
          * 构建类目轴为水平方向的K线图系列
          */
-        function _buildHorizontal(maxDataLength, locationMap) {
+        _buildHorizontal : function (maxDataLength, locationMap) {
+            var series = this.series;
             // 确定类目轴和数值轴，同一方向随便找一个即可
             var seriesIndex;
             var serie;
@@ -151,7 +156,7 @@ define(function (require) {
                 serie = series[seriesIndex];
                 
                 xAxisIndex = serie.xAxisIndex || 0;
-                categoryAxis = component.xAxis.getAxis(xAxisIndex);
+                categoryAxis = this.component.xAxis.getAxis(xAxisIndex);
                 candleWidth = serie.barWidth 
                               || Math.floor(categoryAxis.getGap() / 2);
                 barMaxWidth = serie.barMaxWidth;
@@ -159,7 +164,7 @@ define(function (require) {
                     candleWidth = barMaxWidth;
                 }
                 yAxisIndex = serie.yAxisIndex || 0;
-                valueAxis = component.yAxis.getAxis(yAxisIndex);
+                valueAxis = this.component.yAxis.getAxis(yAxisIndex);
                 
                 pointList[seriesIndex] = [];
                 for (var i = 0, l = maxDataLength; i < l; i++) {
@@ -193,13 +198,14 @@ define(function (require) {
                 }
             }
             // console.log(pointList)
-            _buildKLine(pointList);
-        }
+            this._buildKLine(pointList);
+        },
 
         /**
          * 生成K线
          */
-        function _buildKLine(pointList) {
+        _buildKLine : function (pointList) {
+            var series = this.series;
             // normal:
             var nLineWidth;
             var nLineColor;
@@ -232,35 +238,35 @@ define(function (require) {
                 ) {
                     // 多级控制
                     queryTarget = serie;
-                    nLineWidth = self.query(
+                    nLineWidth = this.query(
                         queryTarget, 'itemStyle.normal.lineStyle.width'
                     );
-                    nLineColor = self.query(
+                    nLineColor = this.query(
                         queryTarget, 'itemStyle.normal.lineStyle.color'
                     );
-                    nLineColor0 = self.query(
+                    nLineColor0 = this.query(
                         queryTarget, 'itemStyle.normal.lineStyle.color0'
                     );
-                    nColor = self.query(
+                    nColor = this.query(
                         queryTarget, 'itemStyle.normal.color'
                     );
-                    nColor0 = self.query(
+                    nColor0 = this.query(
                         queryTarget, 'itemStyle.normal.color0'
                     );
                     
-                    eLineWidth = self.query(
+                    eLineWidth = this.query(
                         queryTarget, 'itemStyle.emphasis.lineStyle.width'
                     );
-                    eLineColor = self.query(
+                    eLineColor = this.query(
                         queryTarget, 'itemStyle.emphasis.lineStyle.color'
                     );
-                    eLineColor0 = self.query(
+                    eLineColor0 = this.query(
                         queryTarget, 'itemStyle.emphasis.lineStyle.color0'
                     );
-                    eColor = self.query(
+                    eColor = this.query(
                         queryTarget, 'itemStyle.emphasis.color'
                     );
-                    eColor0 = self.query(
+                    eColor0 = this.query(
                         queryTarget, 'itemStyle.emphasis.color0'
                     );
 
@@ -281,7 +287,7 @@ define(function (require) {
                         data = serie.data[singlePoint[6]];
                         queryTarget = data;
                         candleType = singlePoint[3] < singlePoint[2];
-                        self.shapeList.push(_getCandle(
+                        this.shapeList.push(this._getCandle(
                             seriesIndex,    // seriesIndex
                             singlePoint[6], // dataIndex
                             singlePoint[7], // name
@@ -295,25 +301,25 @@ define(function (require) {
                             
                             // 填充颜色
                             candleType
-                            ? (self.query(          // 阳
+                            ? (this.query(          // 阳
                                    queryTarget, 'itemStyle.normal.color'
                                ) || nColor)
-                            : (self.query(          // 阴
+                            : (this.query(          // 阴
                                    queryTarget, 'itemStyle.normal.color0'
                                ) || nColor0),
                             
                             // 线宽
-                            self.query(
+                            this.query(
                                queryTarget, 'itemStyle.normal.lineStyle.width'
                             ) || nLineWidth,
                             
                             // 线色
                             candleType
-                            ? (self.query(          // 阳
+                            ? (this.query(          // 阳
                                    queryTarget,
                                    'itemStyle.normal.lineStyle.color'
                                ) || nLineColor)
-                            : (self.query(          // 阴
+                            : (this.query(          // 阴
                                    queryTarget,
                                    'itemStyle.normal.lineStyle.color0'
                                ) || nLineColor0),
@@ -322,25 +328,25 @@ define(function (require) {
                             
                             // 填充颜色
                             candleType
-                            ? (self.query(          // 阳
+                            ? (this.query(          // 阳
                                    queryTarget, 'itemStyle.emphasis.color'
                                ) || eColor || nColor)
-                            : (self.query(          // 阴
+                            : (this.query(          // 阴
                                    queryTarget, 'itemStyle.emphasis.color0'
                                ) || eColor0 || nColor0),
                             
                             // 线宽
-                            self.query(
+                            this.query(
                                queryTarget, 'itemStyle.emphasis.lineStyle.width'
                             ) || eLineWidth || nLineWidth,
                             
                             // 线色
                             candleType
-                            ? (self.query(          // 阳
+                            ? (this.query(          // 阳
                                    queryTarget,
                                    'itemStyle.emphasis.lineStyle.color'
                                ) || eLineColor || nLineColor)
-                            : (self.query(          // 阴
+                            : (this.query(          // 阴
                                    queryTarget,
                                    'itemStyle.emphasis.lineStyle.color0'
                                ) || eLineColor0 || nLineColor0)
@@ -348,20 +354,21 @@ define(function (require) {
                     }
                 }
             }
-            // console.log(self.shapeList)
-        }
+            // console.log(this.shapeList)
+        },
 
         /**
          * 生成K线图上的图形
          */
-        function _getCandle(
+        _getCandle : function (
             seriesIndex, dataIndex, name, 
             x, width, y0, y1, y2, y3, 
             nColor, nLinewidth, nLineColor, 
             eColor, eLinewidth, eLineColor
         ) {
+            var series = this.series;
             var itemShape = {
-                zlevel : _zlevelBase,
+                zlevel : this._zlevelBase,
                 clickable: true,
                 style : {
                     x : x,
@@ -388,12 +395,12 @@ define(function (require) {
             
             itemShape = new CandleShape(itemShape);
             return itemShape;
-        }
+        },
 
         // 位置转换
-        function getMarkCoord(serie, seriesIndex, mpData) {
-            var xAxis = component.xAxis.getAxis(serie.xAxisIndex);
-            var yAxis = component.yAxis.getAxis(serie.yAxisIndex);
+        getMarkCoord : function (serie, seriesIndex, mpData) {
+            var xAxis = this.component.xAxis.getAxis(serie.xAxisIndex);
+            var yAxis = this.component.yAxis.getAxis(serie.yAxisIndex);
             
             return [
                 typeof mpData.xAxis != 'string' 
@@ -406,34 +413,36 @@ define(function (require) {
                 ? yAxis.getCoordByIndex(mpData.yAxis || 0)
                 : yAxis.getCoord(mpData.yAxis || 0)
             ];
-        }
+        },
         
         /**
          * 构造函数默认执行的初始化方法，也用于创建实例后动态修改
          * @param {Object} newSeries
          * @param {Object} newComponent
          */
-        function init(newOption, newComponent) {
-            component = newComponent;
-            refresh(newOption);
-        }
+        init : function (newOption, newComponent) {
+            this.component = newComponent || this.component;
+            
+            this.refresh(newOption);
+        },
 
         /**
          * 刷新
          */
-        function refresh(newOption) {
+        refresh : function (newOption) {
             if (newOption) {
-                option = newOption;
-                series = option.series;
+                this.option = newOption;
+                this.series = newOption.series;
             }
-            self.clear();
-            _buildShape();
-        }
+            this.clear();
+            this._buildShape();
+        },
 
         /**
          * 动画设定
          */
-        function addDataAnimation(params) {
+        addDataAnimation : function (params) {
+            var series = this.series;
             var aniMap = {}; // seriesIndex索引参数
             for (var i = 0, l = params.length; i < l; i++) {
                 aniMap[params[i][0]] = params[i];
@@ -444,31 +453,31 @@ define(function (require) {
             var serie;
             var seriesIndex;
             var dataIndex;
-             for (var i = 0, l = self.shapeList.length; i < l; i++) {
-                seriesIndex = self.shapeList[i]._seriesIndex;
+             for (var i = 0, l = this.shapeList.length; i < l; i++) {
+                seriesIndex = this.shapeList[i]._seriesIndex;
                 if (aniMap[seriesIndex] && !aniMap[seriesIndex][3]) {
                     // 有数据删除才有移动的动画
-                    if (self.shapeList[i].type == 'candle') {
-                        dataIndex = ecData.get(self.shapeList[i], 'dataIndex');
+                    if (this.shapeList[i].type == 'candle') {
+                        dataIndex = ecData.get(this.shapeList[i], 'dataIndex');
                         serie = series[seriesIndex];
                         if (aniMap[seriesIndex][2] 
                             && dataIndex == serie.data.length - 1
                         ) {
                             // 队头加入删除末尾
-                            zr.delShape(self.shapeList[i].id);
+                            this.zr.delShape(this.shapeList[i].id);
                             continue;
                         }
                         else if (!aniMap[seriesIndex][2] && dataIndex === 0) {
                             // 队尾加入删除头部
-                            zr.delShape(self.shapeList[i].id);
+                            this.zr.delShape(this.shapeList[i].id);
                             continue;
                         }
-                        dx = component.xAxis.getAxis(
+                        dx = this.component.xAxis.getAxis(
                                 serie.xAxisIndex || 0
                              ).getGap();
                         x = aniMap[seriesIndex][2] ? dx : -dx;
                         y = 0;
-                        zr.animate(self.shapeList[i].id, '')
+                        this.zr.animate(this.shapeList[i].id, '')
                             .when(
                                 500,
                                 {position : [x, y]}
@@ -477,53 +486,44 @@ define(function (require) {
                     }
                 }
             }
-        }
+        },
         
         /**
          * 动画设定
          */
-        function animation() {
-            var duration = self.query(option, 'animationDuration');
-            var easing = self.query(option, 'animationEasing');
+        animation : function () {
+            var series = this.series;
+            var duration = this.query(this.option, 'animationDuration');
+            var easing = this.query(this.option, 'animationEasing');
             var x;
             var y;
             var serie;
 
-            for (var i = 0, l = self.shapeList.length; i < l; i++) {
-                if (self.shapeList[i].type == 'candle') {
-                    serie = series[self.shapeList[i]._seriesIndex];
-                    x = self.shapeList[i].style.x;
-                    y = self.shapeList[i].style.y[0];
-                    zr.modShape(
-                        self.shapeList[i].id,
+            for (var i = 0, l = this.shapeList.length; i < l; i++) {
+                if (this.shapeList[i].type == 'candle') {
+                    serie = series[this.shapeList[i]._seriesIndex];
+                    x = this.shapeList[i].style.x;
+                    y = this.shapeList[i].style.y[0];
+                    this.zr.modShape(
+                        this.shapeList[i].id,
                         { scale : [1, 0, x, y] },
                         true
                     );
-                    zr.animate(self.shapeList[i].id, '')
+                    this.zr.animate(this.shapeList[i].id, '')
                         .when(
-                            (self.query(serie,'animationDuration')
+                            (this.query(serie,'animationDuration')
                             || duration),
                             {scale : [1, 1, x, y]}
                         )
                         .start(
-                            self.query(serie, 'animationEasing') || easing
+                            this.query(serie, 'animationEasing') || easing
                         );
                 }
             }
             
-            self.animationMark(duration, easing);
+            this.animationMark(duration, easing);
         }
-
-        // 重载基类方法
-        self.getMarkCoord = getMarkCoord;
-        self.animation = animation;
-        
-        self.init = init;
-        self.refresh = refresh;
-        self.addDataAnimation = addDataAnimation;
-
-        init(option, component);
-    }
+    };
     
     zrUtil.inherits(K, CalculableBase);
     zrUtil.inherits(K, ComponentBase);
