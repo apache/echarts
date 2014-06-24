a90cdb632b94a0d981dbdb8f1dfb4cbe6032c356
erik <erik168@163.com>
2014-06-24 11:25:50 +0800

    little optimize

--- src/util/number.js
+++ src/util/number.js
@@ -7,22 +7,22 @@
  */
 define(function() {
     function _trim(str) {
-        return str.replace(/^\s\s*/, '').replace(/\s\s*$/, '');
+        return str.replace(/^\s+/, '').replace(/\s+$/, '');
     }
     
     /**
      * 百分比计算
      */
     function parsePercent(value, maxValue) {
-        if (typeof(value) === 'string') {
+        if (typeof value === 'string') {
             if (_trim(value).match(/%$/)) {
                 return parseFloat(value) / 100 * maxValue;
-            } else {
-                return parseFloat(value);
             }
-        } else {
-            return value;
+
+            return parseFloat(value);
         }
+
+        return value;
     }
     
     /**
