5dbc3fcdde4707aaecf3fad57908a17bc7a4a439
kener <kener.linfeng@gmail.com>
2014-07-24 17:01:23 +0800

    例子更新

--- doc/example/timelineOption.js
+++ doc/example/timelineOption.js
@@ -3,7 +3,7 @@ function dataFormatter(obj) {
     var pList = ['北京','天津','河北','山西','内蒙古','辽宁','吉林','黑龙江','上海','江苏','浙江','安徽','福建','江西','山东','河南','湖北','湖南','广东','广西','海南','重庆','四川','贵州','云南','西藏','陕西','甘肃','青海','宁夏','新疆'];
     var temp;
     var max = 0;
-    for (var year in obj) {
+    for (var year = 2002; year <= 2011; year++) {
         temp = obj[year];
         for (var i = 0, l = temp.length; i < l; i++) {
             max = Math.max(max, temp[i]);
