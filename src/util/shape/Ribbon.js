7475bbacea6711c0f40e2da66940485003db64db
lang <bm2736892@gmail.com>
2014-09-10 23:01:07 +0800

    Chord的RibbonShape使用 pathProxy, IE8支持hover

--- src/util/shape/Ribbon.js
+++ src/util/shape/Ribbon.js
@@ -1,44 +1,47 @@
 /**
- * zrender
- *
+ * @module echarts/util/shape/Ribbon
  * @author pissang (https://github.com/pissang)
- *
- * shape类：chord
- * 可配图形属性：
-   {
-       // 基础属性
-       shape  : 'chord',       // 必须，shape类标识，需要显式指定
-       id     : {string},       // 必须，图形唯一标识，可通过'zrender/tool/guid'方法生成
-       zlevel : {number},       // 默认为0，z层level，决定绘画在哪层canvas中
-       invisible : {boolean},   // 默认为false，是否可见
-
-       // 样式属性，默认状态样式样式属性
-       style  : {
-           center        : {array},
-           source0       : {number},
-           source1       : {number},
-           target0       : {number},
-           target1       : {number},
-           r             : {number},
-       },
-
-       // 样式属性，高亮样式属性，当不存在highlightStyle时使用基于默认样式扩展显示
-       highlightStyle : {
-           // 同style
-       }
-
-       // 交互属性，详见shape.Base
-
-       // 事件属性，详见shape.Base
-   }
+ */
+/**
+ * @typedef {Object} IRibbonStyle
+ * @property {number} x
+ * @property {number} y
+ * @property {number} source0
+ * @property {number} source1
+ * @property {number} target0
+ * @property {number} target1
+ * @property {number} r
+ * @property {string} [brushType='fill']
+ * @property {string} [color='#000000'] 填充颜色
+ * @property {string} [strokeColor='#000000'] 描边颜色
+ * @property {string} [lineCape='butt'] 线帽样式，可以是 butt, round, square
+ * @property {number} [lineWidth=1] 描边宽度
+ * @property {number} [opacity=1] 绘制透明度
+ * @property {number} [shadowBlur=0] 阴影模糊度，大于0有效
+ * @property {string} [shadowColor='#000000'] 阴影颜色
+ * @property {number} [shadowOffsetX=0] 阴影横向偏移
+ * @property {number} [shadowOffsetY=0] 阴影纵向偏移
+ * @property {string} [text] 图形中的附加文本
+ * @property {string} [textColor='#000000'] 文本颜色
+ * @property {string} [textFont] 附加文本样式，eg:'bold 18px verdana'
+ * @property {string} [textPosition='end'] 附加文本位置, 可以是 inside, left, right, top, bottom
+ * @property {string} [textAlign] 默认根据textPosition自动设置，附加文本水平对齐。
+ *                                可以是start, end, left, right, center
+ * @property {string} [textBaseline] 默认根据textPosition自动设置，附加文本垂直对齐。
+ *                                可以是top, bottom, middle, alphabetic, hanging, ideographic
  */
 define(function (require) {
     var Base = require('zrender/shape/Base');
+    var PathProxy = require('zrender/shape/util/PathProxy');
     var zrUtil = require('zrender/tool/util');
+    var area = require('zrender/tool/area');
+
     var _ctx = zrUtil.getContext();
     
     function RibbonShape(options) {
         Base.call(this, options);
+
+        this._pathProxy = new PathProxy();
     }
 
     RibbonShape.prototype = {
@@ -46,9 +49,13 @@ define(function (require) {
         
         // center, source0, source1, target0, target1, r
         buildPath : function (ctx, style) {
+
+            var path = this._pathProxy;
+            path.begin(ctx);
+
             var PI2 = Math.PI * 2;
-            var cx = style.center[0];
-            var cy = style.center[1];
+            var cx = style.x;
+            var cy = style.y;
             var r = style.r;
             var s0 = style.source0 / 180 * Math.PI;
             var s1 = style.source1 / 180 * Math.PI;
@@ -63,9 +70,9 @@ define(function (require) {
             var tx1 = cx + Math.cos(PI2 - t1) * r;
             var ty1 = cy - Math.sin(PI2 - t1) * r;
 
-            ctx.moveTo(sx0, sy0);
-            ctx.arc(cx, cy, style.r, s0, s1, false);
-            ctx.bezierCurveTo(
+            path.moveTo(sx0, sy0);
+            path.arc(cx, cy, style.r, s0, s1, false);
+            path.bezierCurveTo(
                 (cx - sx1) * 0.70 + sx1, 
                 (cy - sy1) * 0.70 + sy1,
                 (cx - tx0) * 0.70 + tx0, 
@@ -73,12 +80,13 @@ define(function (require) {
                 tx0, ty0
             );
             // Chord to self
-            if (style.source0 === style.target0 &&
-                style.source1 === style.target1) {
+            if (style.source0 === style.target0
+                && style.source1 === style.target1
+            ) {
                 return;
             }
-            ctx.arc(cx, cy, style.r, t0, t1, false);
-            ctx.bezierCurveTo(
+            path.arc(cx, cy, style.r, t0, t1, false);
+            path.bezierCurveTo(
                 (cx - tx1) * 0.70 + tx1, 
                 (cy - ty1) * 0.70 + ty1,
                 (cx - sx0) * 0.70 + sx0, 
@@ -87,28 +95,27 @@ define(function (require) {
             );
         },
         
-        getRect : function (){
-            return {
-                x : 0,
-                y : 0,
-                width : 0,
-                height : 0
-            };
+        getRect : function (style) {
+            if (style.__rect) {
+                return style.__rect;
+            }
+            if (!this._pathProxy.isEmpty()) {
+                this.buildPath(null, style);
+            }
+            return this._pathProxy.fastBoundingRect();
         },
                 
         isCover : function (x, y) {
-            if (!_ctx.isPointInPath) {  // In ie
-                return false;
+            var rect = this.getRect(this.style);
+            if (x >= rect.x
+                && x <= (rect.x + rect.width)
+                && y >= rect.y
+                && y <= (rect.y + rect.height)
+            ) {
+                return area.isInsidePath(
+                    this._pathProxy.pathCommands, 0, 'fill', x, y
+                );
             }
-            var originPos = this.getTansform(x, y);
-            x = originPos[0];
-            y = originPos[1];
-            
-            _ctx.beginPath();
-            RibbonShape.prototype.buildPath.call(null, _ctx, this.style);
-            _ctx.closePath();
-            
-            return _ctx.isPointInPath(x, y);
         }
     };
 
