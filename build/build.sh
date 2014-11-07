f33d6dc69cf9d423db3171682771407837e3f675
kener <kener.linfeng@gmail.com>
2014-11-07 18:40:37 +0800

    删除旧的打包策略

--- build/build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-node build.js optimize=true plain=false exclude=map output=echarts.js
-node build.js optimize=false plain=false exclude=map output=echarts-original.js
-node build.js optimize=true plain=false output=echarts-map.js
-node build.js optimize=false plain=false output=echarts-original-map.js
-node build.js optimize=true plain=true exclude=map output=echarts-plain.js
-node build.js optimize=true plain=true output=echarts-plain-map.js
-node build.js optimize=false plain=true exclude=map output=echarts-plain-original.js
-node build.js optimize=false plain=true output=echarts-plain-original-map.js
