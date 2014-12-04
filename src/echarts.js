11421a1eccd45b7c7b28c84bce21a319c6ad1060
kener <kener.linfeng@gmail.com>
2014-12-04 14:36:20 +0800

    update version 2.1.9

--- src/echarts.js
+++ src/echarts.js
@@ -27,9 +27,9 @@ define(function (require) {
     var _instances = {};    // ECharts实例map索引
     var DOM_ATTRIBUTE_KEY = '_echarts_instance_';
     
-    self.version = '2.1.8';
+    self.version = '2.1.9';
     self.dependencies = {
-        zrender: '2.0.5'
+        zrender: '2.0.6'
     };
     /**
      * 入口方法 
