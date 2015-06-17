df0272055c1c6c62f9b628aee816a8d6bd2592d8
lang <bm2736892@gmail.com>
2015-06-17 08:18:43 +0800

    Dump 2.2.4

--- src/echarts.js
+++ src/echarts.js
@@ -27,7 +27,7 @@ define(function (require) {
     var _instances = {};    // ECharts实例map索引
     var DOM_ATTRIBUTE_KEY = '_echarts_instance_';
 
-    self.version = '2.2.3';
+    self.version = '2.2.4';
     self.dependencies = {
         zrender: '2.0.9'
     };
