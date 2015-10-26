2340a3a4c0759a0dbd30b19ff6fc6b2fbb4c97f6
lang <bm2736892@gmail.com>
2015-10-26 18:57:54 +0800

    Default color visual coding fix when has `itemStyle.normal.color` in a single data item

--- src/visual/defaultColor.js
+++ src/visual/defaultColor.js
@@ -7,6 +7,15 @@ define(function (require) {
             var color = seriesModel.get('itemStyle.normal.color') // Set in itemStyle
                 || colorList[seriesModel.seriesIndex];  // Default color
             data.setVisual('color', color);
+
+            data.each(function (idx) {
+                var itemModel = data.getItemModel(idx);
+                var color = itemModel.get('itemStyle.normal.color');
+
+                if (color != null) {
+                    data.setItemVisual(idx, 'color', color);
+                }
+            });
         });
     }
 });
