e37b84bbb309b6fa6fc68b46b571ebd59cbc2b24
lang <bm2736892@gmail.com>
2015-11-27 16:17:48 +0800

    Pie percent to number

--- src/chart/pie/PieSeries.js
+++ src/chart/pie/PieSeries.js
@@ -47,7 +47,7 @@ define(function(require) {
             var data = this._data;
             var params = seriesModelProto.getDataParams.call(this, dataIndex);
             // FIXME toFixed?
-            params.percent = (data.get('value', dataIndex) / data.getSum('value') * 100).toFixed(2);
+            params.percent = +(data.get('value', dataIndex) / data.getSum('value') * 100).toFixed(2);
 
             params.$vars.push('percent');
             return params;
