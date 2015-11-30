1b6aa5c21c3f907e744f8ea041801cc718c7dd81
lang <bm2736892@gmail.com>
2015-11-30 13:59:27 +0800

    Update build wrap

--- build/wrap/start.js
+++ build/wrap/start.js
@@ -1,12 +1,14 @@
 (function (root, factory) {
     if (typeof define === 'function' && define.amd) {
         // AMD. Register as an anonymous module.
-        define(['exports'], factory);
-    } else if (typeof exports === 'object' && typeof exports.nodeName !== 'string') {
-        // CommonJS
-        factory(exports, require('b'));
+        define([], factory);
+    } else if (typeof module === 'object' && module.exports) {
+        // Node. Does not work with strict CommonJS, but
+        // only CommonJS-like environments that support module.exports,
+        // like Node.
+        module.exports = factory();
     } else {
-        // Browser globals
-        factory(root);
+        // Browser globals (root is window)
+        root.echarts = factory();
     }
-}(this, function (exports) {
+}(this, function () {
