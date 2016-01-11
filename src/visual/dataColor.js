396b7be651677234d59bef1ff8ed3287278db206
lang <bm2736892@gmail.com>
2016-01-11 12:14:09 +0800

    Fix #2335, callback in itemStyle

--- src/visual/dataColor.js
+++ src/visual/dataColor.js
@@ -10,11 +10,14 @@ define(function (require) {
                 data.each(function (idx) {
                     var itemModel = data.getItemModel(idx);
                     var rawIdx = data.getRawIndex(idx);
-                    var color = itemModel.get('itemStyle.normal.color')
-                        || colorList[rawIdx % colorList.length];
-                    // Legend may use the visual info in data before processed
-                    dataAll.setItemVisual(rawIdx, 'color', color);
-                    data.setItemVisual(idx, 'color', color);
+                    // If series.itemStyle.normal.color is a function. itemVisual may be encoded
+                    if (!data.getItemVisual(idx, 'color', true)) {
+                        var color = itemModel.get('itemStyle.normal.color')
+                            || colorList[rawIdx % colorList.length];
+                        // Legend may use the visual info in data before processed
+                        dataAll.setItemVisual(rawIdx, 'color', color);
+                        data.setItemVisual(idx, 'color', color);
+                    }
                 });
             }
         });
