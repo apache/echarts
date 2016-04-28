dd7ad217155cca3cd3f4a515351c514c15b78eb1
lang <bm2736892@gmail.com>
2016-04-28 13:48:18 +0800

    Fix markPoint x and y can't  use percent string bug. #3098

--- src/component/marker/markerHelper.js
+++ src/component/marker/markerHelper.js
@@ -79,7 +79,9 @@ define(function (require) {
         // 1. If not specify the position with pixel directly
         // 2. If `coord` is not a data array. Which uses `xAxis`,
         // `yAxis` to specify the coord on each dimension
-        if ((isNaN(item.x) || isNaN(item.y))
+
+        // parseFloat first because item.x and item.y can be percent string like '20%'
+        if ((isNaN(parseFloat(item.x)) || isNaN(parseFloat(item.y)))
             && !zrUtil.isArray(item.coord)
             && coordSys
         ) {
