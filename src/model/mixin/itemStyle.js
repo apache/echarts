f8987a3d8eb7ddc7793c900368e5cb95a3aba8d7
lang <bm2736892@gmail.com>
2016-06-06 11:34:52 +0800

    itemStyle add borderType property

--- src/model/mixin/itemStyle.js
+++ src/model/mixin/itemStyle.js
@@ -1,16 +1,28 @@
 define(function (require) {
+    var getItemStyle = require('./makeStyleMapper')(
+        [
+            ['fill', 'color'],
+            ['stroke', 'borderColor'],
+            ['lineWidth', 'borderWidth'],
+            ['opacity'],
+            ['shadowBlur'],
+            ['shadowOffsetX'],
+            ['shadowOffsetY'],
+            ['shadowColor']
+        ]
+    );
     return {
-        getItemStyle: require('./makeStyleMapper')(
-            [
-                ['fill', 'color'],
-                ['stroke', 'borderColor'],
-                ['lineWidth', 'borderWidth'],
-                ['opacity'],
-                ['shadowBlur'],
-                ['shadowOffsetX'],
-                ['shadowOffsetY'],
-                ['shadowColor']
-            ]
-        )
+        getItemStyle: function (excludes) {
+            var style = getItemStyle.call(this, excludes);
+            var lineDash = this.getBorderLineDash();
+            lineDash && (style.lineDash = lineDash);
+            return style;
+        },
+
+        getBorderLineDash: function () {
+            var lineType = this.get('borderType');
+            return (lineType === 'solid' || lineType == null) ? null
+                : (lineType === 'dashed' ? [5, 5] : [1, 1]);
+        }
     };
 });
