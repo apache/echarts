412c2e9e413084f8aabf940ec6e418544b08cc5d
lang <bm2736892@gmail.com>
2016-07-15 14:54:42 +0800

    Only have default grid component when needed

--- src/component/grid.js
+++ src/component/grid.js
@@ -3,13 +3,14 @@ define(function(require) {
 
     var graphic = require('../util/graphic');
     var zrUtil = require('zrender/core/util');
+    var echarts = require('../echarts');
 
     require('../coord/cartesian/Grid');
 
     require('./axis');
 
     // Grid view
-    require('../echarts').extendComponentView({
+    echarts.extendComponentView({
 
         type: 'grid',
 
@@ -24,6 +25,13 @@ define(function(require) {
                     silent: true
                 }));
             }
+        }
+    });
+
+    echarts.registerPreprocessor(function (option) {
+        // Only create grid when need
+        if (option.xAxis && option.yAxis && !option.grid) {
+            option.grid = {};
         }
     });
 });
