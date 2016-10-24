6c4cb5ab9b1e7cd35a8b5d91c3e1471a48ea71a2
100pah <sushuang0322@gmail.com>
2016-10-24 20:19:33 +0800

    tweak

--- src/chart/map/mapSymbolLayout.js
+++ src/chart/map/mapSymbolLayout.js
@@ -22,10 +22,9 @@ define(function (require) {
                         var name = data.getName(idx);
                         var region = geo.getRegion(name);
 
-                        // No region or no value
-                        // In MapSeries data regions will be filled with NaN
-                        // If they are not in the series.data array.
-                        // So here must validate if value is NaN
+                        // If input series.data is [11, 22, '-'/null/undefined, 44],
+                        // it will be filled with NaN: [11, 22, NaN, 44] and NaN will
+                        // not be drawn. So here must validate if value is NaN.
                         if (!region || isNaN(value)) {
                             return;
                         }
