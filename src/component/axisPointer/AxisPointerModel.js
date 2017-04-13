29201eff5e9efcd5e983166581a1eb73fba20028
pah100 <sushuang0322@gmail.com>
2017-04-13 11:45:35 +0800

    update axispointer path

--- src/component/axisPointer/AxisPointerModel.js
+++ src/component/axisPointer/AxisPointerModel.js
@@ -76,7 +76,7 @@ define(function(require) {
 
             handle: {
                 show: false,
-                icon: 'M10.7,11.9v-1.3H9.3v1.3c-4.9,0.3-8.8,4.4-8.8,9.4c0,5,3.9,9.1,8.8,9.4h1.3c4.9-0.3,8.8-4.4,8.8-9.4C19.5,16.3,15.6,12.2,10.7,11.9z M13.3,24.4H6.7V23h6.6V24.4z M13.3,19.6H6.7v-1.4h6.6V19.6z', // jshint ignore:line
+                icon: 'M10.7,11.9v-1.3H9.3v1.3c-4.9,0.3-8.8,4.4-8.8,9.4c0,5,3.9,9.1,8.8,9.4h1.3c4.9-0.3,8.8-4.4,8.8-9.4C19.5,16.3,15.6,12.2,10.7,11.9z M13.3,24.4H6.7v-1.2h6.6z M13.3,22H6.7v-1.2h6.6z M13.3,19.6H6.7v-1.2h6.6z', // jshint ignore:line
                 size: 45,
                 // handle margin is from symbol center to axis, which is stable when circular move.
                 margin: 50,
