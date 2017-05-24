c4c2233367d23eb2f12d00e43a309e93cba56539
pah100 <sushuang0322@gmail.com>
2017-05-25 03:54:49 +0800

    tweak

--- src/component/dataZoom/InsideZoomModel.js
+++ src/component/dataZoom/InsideZoomModel.js
@@ -14,7 +14,8 @@ define(function(require) {
             disabled: false,   // Whether disable this inside zoom.
             zoomLock: false,   // Whether disable zoom but only pan.
             zoomOnMouseWheel: true, // Can be: true / false / 'shift' / 'ctrl' / 'alt'.
-            moveOnMouseMove: true   // Can be: true / false / 'shift' / 'ctrl' / 'alt'.
+            moveOnMouseMove: true,   // Can be: true / false / 'shift' / 'ctrl' / 'alt'.
+            preventDefaultMouseMove: true
         }
     });
 });
