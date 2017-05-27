eae4fd56b4f25077f64aab167a65daa1b593eaaa
pah100 <sushuang0322@gmail.com>
2017-05-27 17:48:57 +0800

    Fix #5837 (tooltip did not work in large scatter when zoomed)

--- src/chart/helper/LargeSymbolDraw.js
+++ src/chart/helper/LargeSymbolDraw.js
@@ -22,6 +22,11 @@ define(function (require) {
             var symbolProxyShape = symbolProxy.shape;
             for (var i = 0; i < points.length; i++) {
                 var pt = points[i];
+
+                if (isNaN(pt[0]) || isNaN(pt[1])) {
+                    continue;
+                }
+
                 var size = sizes[i];
                 if (size[0] < 4) {
                     // Optimize for small symbol
@@ -121,7 +126,7 @@ define(function (require) {
         symbolEl.on('mousemove', function (e) {
             symbolEl.dataIndex = null;
             var dataIndex = symbolEl.findDataIndex(e.offsetX, e.offsetY);
-            if (dataIndex > 0) {
+            if (dataIndex >= 0) {
                 // Provide dataIndex for tooltip
                 symbolEl.dataIndex = dataIndex;
             }
