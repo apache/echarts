b4ef4b2efed8ad53b9054282f795100bb0ff7563
pah100 <sushuang0322@gmail.com>
2017-06-22 15:20:38 +0800

    link gh-pages to 3.

--- src/util/mapData/geoJson/ao_men_geo.js
+++
@@ -1,3 +0,0 @@
-define(function() {
-    return {"type":"FeatureCollection","features":[{"type":"Feature","id":"8200","properties":{"name":"澳门","cp":[113.5715,22.1583],"childNum":1},"geometry":{"type":"Polygon","coordinates":["@@HQFMDIDGBI@E@EEKEGCEIGGEKEMGSEU@CBEDAJAP@F@LBT@JCHMPOdADCFADAB@LFLDFFP@DAB@@AF@D@B@@FBD@FADHBBHAD@FAJ@JEDCJI`gFIJW"],"encodeOffsets":[[116325,22699]]}}],"UTF8Encoding":true};
-});
