d29b907539628cf88be9327bb72f9f6e1efd8a43
sushuang <sushuang0322@gmail.com>
2017-08-14 17:39:30 +0800

    fix the last commit.

---
+++ src/component/legendScroll.js
@@ -0,0 +1,12 @@
+/**
+ * Legend component entry file8
+ */
+define(function (require) {
+
+    require('./legend');
+
+    require('./legend/ScrollableLegendModel');
+    require('./legend/ScrollableLegendView');
+    require('./legend/scrollableLegendAction');
+
+});
