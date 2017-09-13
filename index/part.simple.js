4ec357fef9ede8914bfaff856d1d3b585f2c322f
sushuang <sushuang0322@gmail.com>
2017-09-13 17:03:23 +0800

    Fix path.

--- index/part.simple.js
+++ index/part.simple.js
@@ -1,4 +1,4 @@
-require('./lib/chart/line');
-require('./lib/chart/bar');
-require('./lib/chart/pie');
-require('./lib/component/gridSimple');
+require('../lib/chart/line');
+require('../lib/chart/bar');
+require('../lib/chart/pie');
+require('../lib/component/gridSimple');
