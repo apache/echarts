f14f2376cd34b4e3e4c047e68bbe6dd63bb0db60
pissang <bm2736892@gmail.com>
2017-12-31 21:59:45 +0800

    Optimize effectScatter updateTransform

--- src/chart/effectScatter/EffectScatterView.js
+++ src/chart/effectScatter/EffectScatterView.js
@@ -1,6 +1,7 @@
 import * as echarts from '../../echarts';
 import SymbolDraw from '../helper/SymbolDraw';
 import EffectSymbol from '../helper/EffectSymbol';
+import * as matrix from 'zrender/src/core/matrix';
 
 export default echarts.extendChartView({
 
@@ -11,14 +12,46 @@ export default echarts.extendChartView({
     },
 
     render: function (seriesModel, ecModel, api) {
+        this._removeRoamTransformInPoints(seriesModel);
+
         var data = seriesModel.getData();
         var effectSymbolDraw = this._symbolDraw;
         effectSymbolDraw.updateData(data);
         this.group.add(effectSymbolDraw.group);
     },
 
-    updateLayout: function () {
-        this._symbolDraw.updateLayout();
+    updateTransform: function (seriesModel, ecModel, api) {
+        var coordSys = seriesModel.coordinateSystem;
+        // Must mark group dirty and make sure the incremental layer will be cleared
+        // PENDING
+        this.group.dirty();
+        if (coordSys.getRoamTransform) {
+            this._updateGroupTransform(seriesModel);
+        }
+        else {
+            return {
+                update: true
+            };
+        }
+    },
+
+    _updateGroupTransform: function (seriesModel) {
+        var coordSys = seriesModel.coordinateSystem;
+        if (coordSys && coordSys.getRoamTransform) {
+            this.group.transform = matrix.clone(coordSys.getRoamTransform());
+            this.group.decomposeTransform();
+        }
+    },
+
+    _removeRoamTransformInPoints: function (seriesModel) {
+        var coordSys = seriesModel.coordinateSystem;
+        if (coordSys && coordSys.removeRoamTransformInPoint) {
+            var data = seriesModel.getData();
+            data.each(function (i) {
+                var pt = data.getItemLayout(i);
+                coordSys.removeRoamTransformInPoint(pt);
+            });
+        }
     },
 
     remove: function (ecModel, api) {
