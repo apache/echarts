1a566dea8d585c255b094e925fe5e86243720323
sushuang <sushuang0322@gmail.com>
2018-10-15 23:48:18 +0800

    release 4.2.0-rc.2

--- dist/echarts-en.js
+++ dist/echarts-en.js
@@ -74554,8 +74554,11 @@ function getAxisKey$1(axis) {
  */
 function barLayoutPolar(seriesType, ecModel, api) {
 
-    // var width = api.getWidth();
-    // var height = api.getHeight();
+    // FIXME
+    // Revert becuase it brings bar progressive bug.
+    // The complete fix will be added in the next version.
+    var width = api.getWidth();
+    var height = api.getHeight();
 
     var lastStackCoords = {};
 
