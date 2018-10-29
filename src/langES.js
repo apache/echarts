e9e1081b79c85f6951f229f188828055daa79bec
Darío Hereñú <magallania@gmail.com>
2018-10-29 14:11:36 -0300

    Added spanish translation

---
+++ src/langES.js
@@ -0,0 +1,59 @@
+/*
+Licensed to the Apache Software Foundation (ASF) under one
+or more contributor license agreements.  See the NOTICE file
+distributed with this work for additional information
+regarding copyright ownership.  The ASF licenses this file
+to you under the Apache License, Version 2.0 (the
+"License"); you may not use this file except in compliance
+with the License.  You may obtain a copy of the License at
+*
+http://www.apache.org/licenses/LICENSE-2.0
+*
+Unless required by applicable law or agreed to in writing,
+software distributed under the License is distributed on an
+"AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+KIND, either express or implied.  See the License for the
+specific language governing permissions and limitations
+under the License.
+*/
+
+
+export default {
+toolbox: {
+    brush: {
+        title: {
+            rect: 'Seleccionar Caja',
+            polygon: 'Seleccionar Lazo',
+            lineX: 'Seleccionar horizontalmente',
+            lineY: 'Seleccionar verticalmente',
+            keep: 'Mantener selecciones',
+            clear: 'Eliminar selecciones'
+        }
+    },
+    dataView: {
+        title: 'Visualizar datos',
+        lang: ['Visualizar datos', 'Cerrar', 'Actualizar']
+    },
+    dataZoom: {
+        title: {
+            zoom: 'Enfocar',
+            back: 'Restablecer enfocar'
+        }
+    },
+    magicType: {
+        title: {
+            line: 'Cambiar a gráfico de líneas',
+            bar: 'Cambiar a gráfico de barras',
+            stack: 'Pila',
+            tiled: 'Teja'
+        }
+    },
+    restore: {
+        title: 'Restaurar'
+    },
+    saveAsImage: {
+        title: 'Guardar como imagen',
+        lang: ['Clic derecho para guardar imagen']
+    }
+}
+};
