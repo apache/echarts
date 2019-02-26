5074b63cb900b228e4dc105697e0b82e90fc0c88
yufeng04 <yufeng04@baidu.com>
2019-02-26 17:03:20 +0800

    fix bug #9563

--- src/chart/sunburst/SunburstPiece.js
+++ src/chart/sunburst/SunburstPiece.js
@@ -203,7 +203,7 @@ SunburstPieceProto._updateLabel = function (seriesModel, visualColor, state) {
 
     var text = zrUtil.retrieve(
         seriesModel.getFormattedLabel(
-            this.node.dataIndex, 'normal', null, null, 'label'
+            this.node.dataIndex, status, null, null, 'label'
         ),
         this.node.name
     );
