827917234138e947b425b47aa0ba258cef4a1be9
sushuang <sushuang0322@gmail.com>
2019-04-21 12:45:33 +0800

    Fix that setting `emphasis.lineStyle.type` as solid dose not work. Fix #9704, close #10129.

--- src/model/mixin/lineStyle.js
+++ src/model/mixin/lineStyle.js
@@ -34,8 +34,9 @@ var getLineStyle = makeStyleMapper(
 export default {
     getLineStyle: function (excludes) {
         var style = getLineStyle(this, excludes);
-        var lineDash = this.getLineDash(style.lineWidth);
-        lineDash && (style.lineDash = lineDash);
+        // Always set lineDash whether dashed, otherwise we can not 
+        // erase the previous style when assigning to el.style.
+        style.lineDash = this.getLineDash(style.lineWidth);
         return style;
     },
 
@@ -46,7 +47,15 @@ export default {
         var lineType = this.get('type');
         var dotSize = Math.max(lineWidth, 2);
         var dashSize = lineWidth * 4;
-        return (lineType === 'solid' || lineType == null) ? null
-            : (lineType === 'dashed' ? [dashSize, dashSize] : [dotSize, dotSize]);
+        return (lineType === 'solid' || lineType == null) 
+            // Use `false` but not `null` for the solid line here, because `null` might be 
+            // ignored when assigning to `el.style`. e.g., when setting `lineStyle.type` as 
+            // `'dashed'` and `emphasis.lineStyle.type` as `'solid'` in graph series, the 
+            // `lineDash` gotten form the latter one is not able to erase that from the former 
+            // one if using `null` here according to the emhpsis strategy in `util/graphic.js`.
+            ? false
+            : lineType === 'dashed' 
+            ? [dashSize, dashSize] 
+            : [dotSize, dotSize];
     }
 };
