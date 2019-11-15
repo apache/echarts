9c8cf9619708bddbd5997d62da94ed3511b3262f
Ovilia <zwl.sjtu@gmail.com>
2019-11-15 14:57:52 +0800

    fix(pie): in case there is no truncatedText

--- src/chart/pie/PieView.js
+++ src/chart/pie/PieView.js
@@ -273,7 +273,7 @@ piePieceProto._updateLabel = function (data, idx, withAnimation) {
         {
             labelFetcher: data.hostModel,
             labelDataIndex: idx,
-            defaultText: labelLayout.truncatedText,
+            defaultText: labelLayout.truncatedText == null ? labelLayout.truncatedText : labelLayout.truncatedText,
             autoColor: visualColor,
             useInsideStyle: !!labelLayout.inside
         },
