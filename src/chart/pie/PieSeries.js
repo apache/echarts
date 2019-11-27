380b5244f3f598db31e11bbf77ac35378558455c
Ovilia <zwl.sjtu@gmail.com>
2019-11-27 15:09:14 +0800

    style(pie): rename padding to distanceToLabelLine

--- src/chart/pie/PieSeries.js
+++ src/chart/pie/PieSeries.js
@@ -160,7 +160,7 @@ var PieSeries = echarts.extendSeriesModel({
             // Closest distance between label and chart edge.
             // Works only position is 'outer' and alignTo is 'labelLine' or 'edge'.
             margin: '25%',
-            padding: 5,
+            distanceToLabelLine: 5,
             // formatter: 标签文本格式器，同Tooltip.formatter，不支持异步回调
             // 默认使用全局文本样式，详见TEXTSTYLE
             // distance: 当position为inner时有效，为label位置到圆心的距离与圆半径(环状图为内外半径和)的比例系数
