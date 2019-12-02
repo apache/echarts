257d51be84afddff3c0ba085b6c0805c108d360c
pissang <bm2736892@gmail.com>
2019-12-02 13:04:28 +0800

    fix es3 syntax error

--- src/component/axis/AxisBuilder.js
+++ src/component/axis/AxisBuilder.js
@@ -664,7 +664,7 @@ function buildAxisMinorTicks(axisBuilder, axisModel, opt) {
 
     for (var i = 0; i < minorTicksCoords.length; i++) {
         var minorTicksEls = createTicks(
-            minorTicksCoords[i], axisBuilder._transform, tickEndCoord, minorTickLineStyle, 'minorticks_' + i,
+            minorTicksCoords[i], axisBuilder._transform, tickEndCoord, minorTickLineStyle, 'minorticks_' + i
         );
         for (let k = 0; k < minorTicksEls.length; k++) {
             axisBuilder.group.add(minorTicksEls[k]);
