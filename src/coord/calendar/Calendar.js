b96ab02eca46da4d74736d6c7718dc4ce5eaba5d
fengfeng043 <729964264@qq.com>
2020-01-19 10:20:06 +0800

    remove chinese

--- src/coord/calendar/Calendar.js
+++ src/coord/calendar/Calendar.js
@@ -361,7 +361,6 @@ Calendar.prototype = {
      * @return {Object}       obj
      */
     _getRangeInfo: function (range) {
-        // range中每个元素是个对象
         range = [
             this.getDateInfo(range[0]),
             this.getDateInfo(range[1])
@@ -384,7 +383,6 @@ Calendar.prototype = {
         //     date: date
         // };
 
-        // 跨越了几天，包含头尾
         var allDay = Math.floor(range[1].time / PROXIMATE_ONE_DAY)
             - Math.floor(range[0].time / PROXIMATE_ONE_DAY) + 1;
 
@@ -397,20 +395,15 @@ Calendar.prototype = {
         // ```
         // will get wrong result because of DST. So we should fix it.
         var date = new Date(range[0].time);
-        // 开始的时间是周几
         var startDateNum = date.getDate();
-        // 结束的时间是周几
         var endDateNum = range[1].date.getDate();
-        // 应该结尾的那一天
         date.setDate(startDateNum + allDay - 1);
         // The bias can not over a month, so just compare date.
-        // 不等，则出现DST问题。等则无DST问题
         if (date.getDate() !== endDateNum) {
-            // sign记录调快还是调慢了。三月份那次，
             // I only change '>' to '<'.
             var sign = date.getTime() - range[1].time < 0 ? 1 : -1;
             while (date.getDate() !== endDateNum && (date.getTime() - range[1].time) * sign > 0) {
-                // 目的就是修改这个allday
+                // allday is the final result. It will occur in the 'return'.
                 allDay -= sign;
                 date.setDate(startDateNum + allDay - 1);
             }
