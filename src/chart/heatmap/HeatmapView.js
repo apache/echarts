ae07a9e3e209168a906e64643525c5fa88bad9d2
saniac <snailsword@gmail.com>
2020-03-28 01:17:33 +0800

    fix(heatmap): 1px gap between rect

--- src/chart/heatmap/HeatmapView.js
+++ src/chart/heatmap/HeatmapView.js
@@ -185,8 +185,8 @@ export default echarts.extendChartView({
 
                 rect = new graphic.Rect({
                     shape: {
-                        x: Math.floor(point[0] - width / 2),
-                        y: Math.floor(point[1] - height / 2),
+                        x: Math.floor(Math.round(point[0]) - width / 2),
+                        y: Math.floor(Math.round(point[1]) - height / 2),
                         width: Math.ceil(width),
                         height: Math.ceil(height)
                     },
