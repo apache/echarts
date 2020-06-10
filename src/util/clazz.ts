fbdb003fcab54784463370e38f988c3b5b5c432c
100pah <sushuang0322@gmail.com>
2020-06-10 15:13:53 +0800

    ts: fix es class usage for inheritance.

--- src/util/clazz.ts
+++ src/util/clazz.ts
@@ -99,17 +99,26 @@ export function enableClassExtend(rootClz: ExtendableConstructor, mandatoryMetho
         // constructor.
         // If this constructor/$constructor is declared, it is responsible for
         // calling the super constructor.
-        const ExtendedClass = (class {
-            constructor() {
-                if (!proto.$constructor) {
+        function ExtendedClass(this: any, ...args: any[]) {
+            if (!proto.$constructor) {
+
+                if (!isESClass(superClass)) {
+                    // Will throw error if superClass is an es6 native class.
                     superClass.apply(this, arguments);
                 }
                 else {
-                    proto.$constructor.apply(this, arguments);
+                    const ins = zrUtil.createObject(
+                        // @ts-ignore
+                        ExtendedClass.prototype, new superClass(...args)
+                    );
+                    return ins;
                 }
             }
-            static [IS_EXTENDED_CLASS] = true;
-        }) as ExtendableConstructor;
+            else {
+                proto.$constructor.apply(this, arguments);
+            }
+        }
+        ExtendedClass[IS_EXTENDED_CLASS] = true;
 
         zrUtil.extend(ExtendedClass.prototype, proto);
 
@@ -119,8 +128,13 @@ export function enableClassExtend(rootClz: ExtendableConstructor, mandatoryMetho
         zrUtil.inherits(ExtendedClass, this);
         ExtendedClass.superClass = superClass;
 
-        return ExtendedClass as ExtendableConstructor;
+        return ExtendedClass as unknown as ExtendableConstructor;
     };
+}
+
+function isESClass(fn: unknown): boolean {
+    return typeof fn === 'function'
+        && /^class\s/.test(Function.prototype.toString.call(fn));
 }
 
 /**
