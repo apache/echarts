f2d34e0d5d1c250e23d0de16d978e278e1548149
Ovilia <zwl.sjtu@gmail.com>
2020-07-29 09:56:37 +0800

    feat(time): update lang

--- src/lang.ts
+++ src/lang.ts
@@ -24,12 +24,12 @@
 export default {
     time: {
         month: [
-            'January', 'February', 'March', 'April', 'May', 'June',
-            'July', 'August', 'September', 'October', 'November', 'December'
+            '一月', '二月', '三月', '四月', '五月', '六月',
+            '七月', '八月', '九月', '十月', '十一月', '十二月'
         ],
         monthAbbr: [
-            'Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun',
-            'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'
+            '一', '二', '三', '四', '五', '六',
+            '七', '八', '九', '十', '十一', '十二'
         ],
         dayOfWeek: [
             '星期日', '星期一', '星期二', '星期三', '星期四', '星期五', '星期六'
