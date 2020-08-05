089a3620979d2e42273db930f81c2eb215140519
pissang <bm2736892@gmail.com>
2020-08-05 23:09:02 +0800

    fix(graph): fix null pointer error in graph simple layout

--- src/chart/graph/simpleLayout.js
+++ src/chart/graph/simpleLayout.js
@@ -51,7 +51,7 @@ export default function (ecModel, api) {
                 }
             }
 
-            simpleLayoutEdge(data.graph);
+            simpleLayoutEdge(data.graph, seriesModel);
         }
         else if (!layout || layout === 'none') {
             simpleLayout(seriesModel);
