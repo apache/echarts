786c2bab2362cd0279efa1e1fe16b37aee8844dd
pissang <bm2736892@gmail.com>
2020-10-13 18:08:20 +0800

    fix(bar): not display zero value bar on polar coord.

--- src/chart/bar/BarView.ts
+++ src/chart/bar/BarView.ts
@@ -856,8 +856,6 @@ function updateStyle(
 
     el.useStyle(style);
 
-    el.ignore = isZeroOnPolar(layout as SectorLayout);
-
     const cursorStyle = itemModel.getShallow('cursor');
     cursorStyle && (el as Path).attr('cursor', cursorStyle);
 
@@ -890,6 +888,8 @@ function updateStyle(
     setStatesStylesFromModel(el, itemModel);
 
     if (isZeroOnPolar(layout as SectorLayout)) {
+        el.style.fill = 'none';
+        el.style.stroke = 'none';
         each(el.states, (state) => {
             if (state.style) {
                 state.style.fill = state.style.stroke = 'none';
