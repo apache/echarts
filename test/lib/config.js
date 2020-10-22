8b79d3c0ebb119e1cd06ed3938381808ada63397
100pah <sushuang0322@gmail.com>
2020-10-22 13:34:17 +0800

    test: update ecstat dep.

--- test/lib/config.js
+++ test/lib/config.js
@@ -63,8 +63,8 @@
             paths: {
                 'echarts': ecDistPath,
                 'zrender': 'node_modules/zrender/dist/zrender',
-                // 'ecStat': 'test/lib/ecStat.min.js',
-                'ecStat': 'http://localhost:8001/echarts/echarts-stat/dist/ecStat',
+                'ecStat': 'test/lib/ecStat.min',
+                // 'ecStat': 'http://localhost:8001/echarts/echarts-stat/dist/ecStat',
                 'geoJson': '../geoData/geoJson',
                 'theme': 'theme',
                 'data': 'test/data',
