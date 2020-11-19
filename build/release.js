c3bdb23cf9a37e276d28d442e7b0bdb5c93fbdb4
100pah <sushuang0322@gmail.com>
2020-11-19 20:23:38 +0800

    fix: do not build myTransform when release (asf artefacts has no test lib)

--- build/release.js
+++ build/release.js
@@ -42,7 +42,7 @@ function release() {
         }
     }
 
-    const argsList = ['', 'simple', 'common', 'extension', 'myTransform'].map((type) => {
+    const argsList = ['', 'simple', 'common', 'extension'].map((type) => {
         return [
             '--type',
             type,
