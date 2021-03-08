4878e343065874862a5838b033b4e5705aff887c
leosxie <xjw1987524@163.com>
2021-03-08 12:24:48 +0800

    fix(style): change code style

--- .history/src/model/Series_20210308122052.ts
+++ .history/src/model/Series_20210308122052.ts
@@ -575,7 +575,8 @@ class SeriesModel<Opt extends SeriesOption = SeriesOption> extends ComponentMode
         if (data.hasItemOption) {
             data.each(function (idx) {
                 const rawItem = data.getRawDataItem(idx);
-                if (rawItem && (rawItem as OptionDataItemObject<unknown>).selected) {
+                if (rawItem && (rawItem as OptionDataItemObject<unknown>).selected
+                ) {
                     dataIndices.push(idx);
                 }
             });
