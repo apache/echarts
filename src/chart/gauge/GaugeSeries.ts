e6cdbb6da7630ae0c39a0ba85ec57e42890b0234
pissang <bm2736892@gmail.com>
2021-06-29 18:21:22 +0800

    fix(type): color should be optional

--- src/chart/gauge/GaugeSeries.ts
+++ src/chart/gauge/GaugeSeries.ts
@@ -132,7 +132,7 @@ export interface GaugeSeriesOption extends SeriesOption<GaugeStateOption>, Gauge
         show?: boolean
         roundCap?: boolean
         lineStyle?: Omit<LineStyleOption, 'color'> & {
-            color: GaugeColorStop[]
+            color?: GaugeColorStop[]
         }
     },
 
