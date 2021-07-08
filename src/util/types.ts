d35dda025d632e6f8d4edd386b6b02097f863845
Map1en_ <maplenagisa@gmail.com>
2021-07-08 11:07:22 +0800

    fix: add polarId and rm useless type

--- src/util/types.ts
+++ src/util/types.ts
@@ -1656,13 +1656,8 @@ export interface SeriesOnCartesianOptionMixin {
 }
 
 export interface SeriesOnPolarOptionMixin {
-    radiusAxisIndex?: number
-    angleAxisIndex?: number
-
-    radiusAxisId?: string
-    angleAxisId?: string
-
     polarIndex?: number
+    polarId?: string;
 }
 
 export interface SeriesOnSingleOptionMixin {
