a3d537f0446418e777664016f555f89eb9ac4f57
pissang <bm2736892@gmail.com>
2021-12-16 10:43:19 +0800

    fix(graphic): fix group event not work

--- src/component/graphic/GraphicView.ts
+++ src/component/graphic/GraphicView.ts
@@ -439,18 +439,16 @@ function updateCommonAttrs(
     defaultZ: number,
     defaultZlevel: number
 ) {
-    if (el.isGroup) {
-        return;
+    if (!el.isGroup) {
+        const elDisplayable = el as Displayable;
+        // We should not support configure z and zlevel in the element level.
+        // But seems we didn't limit it previously. So here still use it to avoid breaking.
+        elDisplayable.z = zrUtil.retrieve2((elOption as any).z, defaultZ || 0);
+        elDisplayable.zlevel = zrUtil.retrieve2((elOption as any).zlevel, defaultZlevel || 0);
+        // z2 must not be null/undefined, otherwise sort error may occur.
+        const optZ2 = (elOption as GraphicComponentDisplayableOption).z2;
+        optZ2 != null && (elDisplayable.z2 = optZ2 || 0);
     }
-
-    const elDisplayable = el as Displayable;
-    // We should not support configure z and zlevel in the element level.
-    // But seems we didn't limit it previously. So here still use it to avoid breaking.
-    elDisplayable.z = zrUtil.retrieve2((elOption as any).z, defaultZ || 0);
-    elDisplayable.zlevel = zrUtil.retrieve2((elOption as any).zlevel, defaultZlevel || 0);
-    // z2 must not be null/undefined, otherwise sort error may occur.
-    const optZ2 = (elOption as GraphicComponentDisplayableOption).z2;
-    optZ2 != null && (elDisplayable.z2 = optZ2 || 0);
 
     zrUtil.each(zrUtil.keys(elOption), key => {
         const val = (elOption as any)[key];
