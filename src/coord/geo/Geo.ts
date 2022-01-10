3c7d7a71cfa04a3682723c9ac6caff0935fe7480
pissang <bm2736892@gmail.com>
2022-01-10 16:19:53 +0800

    feat(map): add more projection cases

--- src/coord/geo/Geo.ts
+++ src/coord/geo/Geo.ts
@@ -100,11 +100,20 @@ class Geo extends View {
         this._regionsMap = source.regionsMap;
         this.regions = source.regions;
 
-        if (resourceType === 'geoSVG' && projection) {
-            if (__DEV__) {
-                warn(`Map ${map} with SVG source can't use projection. Only GeoJSON source supports projection.`);
+        if (projection) {
+            // Do some check
+            if (resourceType === 'geoSVG') {
+                if (__DEV__) {
+                    warn(`Map ${map} with SVG source can't use projection. Only GeoJSON source supports projection.`);
+                }
+                projection = null;
             }
-            projection = null;
+            if (!(projection.project && projection.unproject)) {
+                if (__DEV__) {
+                    warn('project and unproject must be both provided in the projeciton.');
+                }
+                projection = null;
+            }
         }
         this.projection = projection;
 
