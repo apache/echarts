3ac05622ec234227d0edae3f263772855073a59e
FrankChencc <370529222@qq.com>
2022-02-11 11:47:24 +0800

    fix(sunburst) set the position of label in center when angle is 2π and r0 is 0. close #16296

--- src/chart/sunburst/SunburstPiece.ts
+++ src/chart/sunburst/SunburstPiece.ts
@@ -1,5 +1,4 @@
 /*
-/*
 * Licensed to the Apache Software Foundation (ASF) under one
 * or more contributor license agreements.  See the NOTICE file
 * distributed with this work for additional information
