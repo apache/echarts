4b11232466d324c981bea804c59608994e8288bb
pissang <bm2736892@gmail.com>
2022-03-23 20:08:05 +0800

    fix wrong import

--- src/component/transform/aggregateTransform.ts
+++ src/component/transform/aggregateTransform.ts
@@ -16,8 +16,6 @@
 * specific language governing permissions and limitations
 * under the License.
 */
-
-import { validate } from '@babel/types';
 import { assert, each, keys, map, retrieve2 } from 'zrender/src/core/util';
 import {
     DataTransformOption,
