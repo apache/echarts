7c029eece63086a8a895c5f5c778711220fe1965
jiawulin001 <jiawulin@vip.qq.com>
2022-04-21 15:56:46 +0800

    remove decal from ITEM_STYLE_KEY_MAP

--- src/model/mixin/itemStyle.ts
+++ src/model/mixin/itemStyle.ts
@@ -35,8 +35,7 @@ export const ITEM_STYLE_KEY_MAP = [
     ['lineDashOffset', 'borderDashOffset'],
     ['lineCap', 'borderCap'],
     ['lineJoin', 'borderJoin'],
-    ['miterLimit', 'borderMiterLimit'],
-    ['decal']
+    ['miterLimit', 'borderMiterLimit']
     // Option decal is in `DecalObject` but style.decal is in `PatternObject`.
     // So do not transfer decal directly.
 ];
