734cfbf4640f107ee79826cdafe95842ae40ec4d
susiwen8 <susiwen8@gmail.com>
2022-06-07 15:36:58 +0800

    chore: remove some unintentional comment

--- src/util/clazz.ts
+++ src/util/clazz.ts
@@ -231,8 +231,8 @@ export function enableClassManagement(
      * Component model classes
      * key: componentType,
      * value:
-     *     componentClass, when componentType is 'xxx'
-     *     or Object.<subKey, componentClass>, when componentType is 'xxx.yy'
+     *     componentClass, when componentType is 'a'
+     *     or Object.<subKey, componentClass>, when componentType is 'a.b'
      */
     const storage: {
         [componentMainType: string]: (Constructor | SubclassContainer)
