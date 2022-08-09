1e7f3b8750fcb977c1dd2d7dc8d198a70deaadd8
liangism <liangism1995@163.com>
2022-08-09 10:02:00 +0800

    fix(theme):  move the label block to the outside.

--- theme/macarons2.js
+++ theme/macarons2.js
@@ -88,17 +88,17 @@
             // barWidth: null,        // 默认自适应
             barGap: '30%', // 柱间距离，默认为柱形宽度的30%，可设固定值
             barCategoryGap: '20%', // 类目间柱形距离，默认为类目间距的20%，可设固定值
+            label: {
+                show: false
+                // position: 默认自适应，水平布局为'top'，垂直布局为'right'，可选为
+                //           'inside'|'left'|'right'|'top'|'bottom'
+                // textStyle: null      // 默认使用全局文本样式，详见TEXTSTYLE
+            },
             itemStyle: {
                 // color: '各异',
                 barBorderColor: '#fff', // 柱条边线
                 barBorderRadius: 0, // 柱条边线圆角，单位px，默认为0
-                barBorderWidth: 1, // 柱条边线线宽，单位px，默认为1
-                label: {
-                    show: false
-                    // position: 默认自适应，水平布局为'top'，垂直布局为'right'，可选为
-                    //           'inside'|'left'|'right'|'top'|'bottom'
-                    // textStyle: null      // 默认使用全局文本样式，详见TEXTSTYLE
-                }
+                barBorderWidth: 1 // 柱条边线线宽，单位px，默认为1
             },
             emphasis: {
                 itemStyle: {
@@ -173,17 +173,17 @@
             startAngle: 90,
             minAngle: 0, // 最小角度改为0
             selectedOffset: 10, // 选中是扇区偏移量
+            label: {
+                show: true,
+                position: 'outer',
+                color: '#1b1b1b',
+                lineStyle: { color: '#1b1b1b' }
+                // textStyle: null      // 默认使用全局文本样式，详见TEXTSTYLE
+            },
             itemStyle: {
                 // color: 各异,
                 borderColor: '#fff',
-                borderWidth: 1,
-                label: {
-                    show: true,
-                    position: 'outer',
-                    color: '#1b1b1b',
-                    lineStyle: { color: '#1b1b1b' }
-                    // textStyle: null      // 默认使用全局文本样式，详见TEXTSTYLE
-                }
+                borderWidth: 1
             },
             labelLine: {
                 show: true,
