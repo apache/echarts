d94f926cefca101065c7aa30b9f111e707da0b61
Konrad Amtenbrink <konrad.amtenbrink@knime.com>
2022-09-07 14:42:11 +0200

    fix: fixed linting + added tests

--- src/chart/boxplot/BoxplotView.ts
+++ src/chart/boxplot/BoxplotView.ts
@@ -146,7 +146,7 @@ function createNormalBox(
     dataIndex: number,
     constDim: number,
     group: ViewRootGroup,
-    isInit?: boolean,
+    isInit?: boolean
 ) {
     const ends = itemLayout.ends;
 
@@ -169,7 +169,7 @@ function updateNormalBoxData(
     data: SeriesData,
     dataIndex: number,
     group: ViewRootGroup,
-    isInit?: boolean,
+    isInit?: boolean
 ) {
     const seriesModel = data.hostModel;
     const updateMethod = graphic[isInit ? 'initProps' : 'updateProps'];
@@ -201,21 +201,28 @@ function updateNormalBoxData(
     toggleHoverEmphasis(el, emphasisModel.get('focus'), emphasisModel.get('blurScope'), emphasisModel.get('disabled'));
 }
 
-function setBoxLabels(boxItemPositions: number[][], formattedLabels: Array<string>, labelOption: any, group: ViewRootGroup) {
+function setBoxLabels(boxItemPositions: number[][], formattedLabels: Array<string>,
+    labelOption: any, group: ViewRootGroup) {
     // get sorted and unique y positions of box items
-    const uniqueYPositions = Array.from(new Set(boxItemPositions.map((pos: number[]) => pos[1])));
-    uniqueYPositions.sort(function(a: number, b: number) {
-        return a - b;
+    const yPositions = boxItemPositions.map((pos: number[]) => pos[1]);
+    const uniqueYPositions: number[] = [];
+    yPositions.forEach(position => {
+        if (!uniqueYPositions.includes(position)) {
+            uniqueYPositions.push(position);
+        }
+    });
+    uniqueYPositions.sort(function (a: number, b: number) {
+        return b - a;
     });
 
-    boxItemPositions.sort(function(a: number[], b: number[]) {
+    boxItemPositions.sort(function (a: number[], b: number[]) {
         return a[0] - b[0];
     });
 
     // get alternating y positions for labels to avoid overlap
     const uniqueAlternatingPositions = uniqueYPositions.map((posY: number, ind: number) => {
-            const matchingPositions = boxItemPositions.filter((orgPos: number[]) => orgPos[1] == posY);
-            const index = (ind % 2 == 0) ? 0 : matchingPositions.length - 1;
+            const matchingPositions = boxItemPositions.filter((orgPos: number[]) => orgPos[1] === posY);
+            const index = (ind % 2 === 0) ? 0 : matchingPositions.length - 1;
             return matchingPositions[index];
     });
 
@@ -228,8 +235,8 @@ function setBoxLabels(boxItemPositions: number[][], formattedLabels: Array<strin
             z2: 1000
         });
         const defaultOffset = 5;
-        const defaultXOffset = (ind % 2) === 0 ? (- (label.getBoundingRect().width + defaultOffset)) : defaultOffset;
-        const defaultYOffset = - defaultOffset;
+        const defaultXOffset = (ind % 2) === 0 ? (-(label.getBoundingRect().width + defaultOffset)) : defaultOffset;
+        const defaultYOffset = -defaultOffset;
         const customOffset = labelOption.offset ? labelOption.offset : [0, 0];
         label.x = uniqueAlternatingPositions[ind][0] + defaultXOffset + customOffset[0],
         label.y = uniqueAlternatingPositions[ind][1] + defaultYOffset + customOffset[1],
