305cedc71f96982760dba7312598b6e0044a50bb
Aman1919 <amansingh33849@gmail.com>
2023-02-19 04:33:50 +0530

    fix decal in aria

--- src/data/Tree.ts
+++ src/data/Tree.ts
@@ -265,6 +265,9 @@ export class TreeNode {
     getVisual(key: string): unknown {
         return this.hostTree.data.getItemVisual(this.dataIndex, key as any);
     }
+     getDecal(key: string): unknown {
+        return this.hostTree.data.getVisual(key as any);
+    }
 
     getRawIndex(): number {
         return this.hostTree.data.getRawIndex(this.dataIndex);
