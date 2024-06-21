9cb5d9a819dcfcb58e46c0b815a912f1d451a897
semla <semla.dev@gmail.com>
2024-06-21 20:52:49 +0200

    test(aria): update test to realistic use case
    that omits lat & long for aria-label

--- test/ut/spec/series/aria.test.ts
+++ test/ut/spec/series/aria.test.ts
@@ -19,133 +19,75 @@
 import { EChartsType } from '@/src/echarts';
 import { createChart, getECModel } from '../../core/utHelper';
 
-describe('omit some aria data', function() {
-
+describe('aria, omit data', function () {
     let chart: EChartsType;
-    beforeEach(function() {
+    const option = {
+        'aria': {
+            'enabled': true,
+            'data': {
+                'columnsToExclude': [0, 1, 2]
+            },
+        },
+        'dataset': [
+            {
+                'dimensions': [
+                    'lng',
+                    'lat',
+                    'name',
+                    'value',
+                    'capacity',
+                ],
+                'source': [
+                    [
+                        1.58285827,
+                        42.099784969,
+                        'Llosa del Cavall (Navès)',
+                        17.945,
+                        80,
+                    ],
+                    [
+                        0.960270444,
+                        41.134931354,
+                        'Riudecanyes',
+                        0.401,
+                        5.32,
+                    ],
+                ]
+
+            }
+        ],
+        'series': [
+            {
+                'coordinateSystem': 'geo',
+                'encode': {
+                    'itemName': 'name'
+                },
+                'type': 'scatter',
+            }
+        ],
+    };
+    beforeEach(function () {
         chart = createChart();
     });
 
-    afterEach(function() {
+    afterEach(function () {
         chart.dispose();
     });
 
-    it('data for column index in columnsToExclude (Tuesday, second column) should be omitted from Aria', async () => {
-        const option = {
-            aria: {
-                enabled: true,
-                data: {
-                    columnsToExclude: [1]
-                }
-            },
-            xAxis: {
-                type: 'category',
-                data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
-            },
-            yAxis: {
-                type: 'value'
-            },
-            series: [
-                {
-                    data: [150, 230, 224, 218, 135, 147, 260],
-                    type: 'line'
-                }
-            ]
-        };
+    it('specified columns should be omitted from Aria (geolocation and name)', () => {
         chart.setOption(option);
         const el = chart.getDom();
         const ariaValue = el.getAttribute('aria-label');
-        expect(ariaValue).not.toContain('Tue');
+        expect(ariaValue).not.toContain(1.58285827);
+        expect(ariaValue).not.toContain(42.099784969);
+        expect(ariaValue).not.toContain(0.960270444);
+        expect(ariaValue).not.toContain(41.134931354);
     });
 
-    it('data for graph should not be omitted', async () => {
-        const option = {
-            aria: {
-                enabled: true,
-                data: {
-                    columnsToExclude: [0]
-                }
-            },
-            xAxis: {
-                type: 'category',
-                data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
-            },
-            yAxis: {
-                type: 'value'
-            },
-            series: [
-                {
-                    data: [150, 230, 224, 218, 135, 147, 260],
-                    type: 'line'
-                }
-            ]
-        };
+    it('should not modify the data of the chart', async () => {
         chart.setOption(option);
         const listData = getECModel(chart).getSeries()[0].getData();
-        expect(listData.getName(0)).toEqual('Mon');
-        expect(listData.getValues(0)).toEqual([0, 150]);
-        expect(listData.count()).toEqual(7);
-    });
-
-    it('aria should be omitted correctly in nested array', async () => {
-        const option = {
-            aria: {
-                enabled: true,
-                data: {
-                    columnsToExclude: [0]
-                }
-            },
-            xAxis: {
-                data: ['2017-10-24', '2017-10-25', '2017-10-26', '2017-10-27']
-            },
-            yAxis: {},
-            series: [
-                {
-                    type: 'candlestick',
-                    data: [
-                        [20, 34, 10, 38],
-                        [40, 35, 30, 50],
-                        [31, 38, 33, 44],
-                        [38, 15, 5, 42]
-                    ]
-                }
-            ]
-        };
-        chart.setOption(option);
-        const el = chart.getDom();
-        const ariaValue = el.getAttribute('aria-label');
-        expect(ariaValue).not.toContain('2017-10-24');
-    });
-
-
-    it('data for graph should not be omitted', async () => {
-        const option = {
-            aria: {
-                enabled: true,
-                data: {
-                    columnsToExclude: [0]
-                }
-            },
-            xAxis: {
-                data: ['2017-10-24', '2017-10-25', '2017-10-26', '2017-10-27']
-            },
-            yAxis: {},
-            series: [
-                {
-                    type: 'candlestick',
-                    data: [
-                        [20, 34, 10, 38],
-                        [40, 35, 30, 50],
-                        [31, 38, 33, 44],
-                        [38, 15, 5, 42]
-                    ]
-                }
-            ]
-        };
-        chart.setOption(option);
-        const listData = getECModel(chart).getSeries()[0].getData();
-        expect(listData.getName(0)).toEqual('2017-10-24');
-        expect(listData.count()).toEqual(4);
+        expect(listData.getValues(0)).toEqual([1.58285827, 42.099784969, 'Llosa del Cavall (Navès)', 17.945, 80]);
     });
 
 });
