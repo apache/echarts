880afcdfd8deca00bcdfd83e8005b775969da149
Najam Shehzad <najambutt195@gmail.com>
2024-08-06 08:51:29 +0200

    fix(i18n): add custom and chart in Swedish translation

--- src/i18n/langSV.ts
+++ src/i18n/langSV.ts
@@ -105,6 +105,8 @@ export default  {
         pictorialBar: 'Bildstapel',
         themeRiver: 'Tematisk flod',
         sunburst: 'Solburstdiagram',
+        custom: 'Anpassat',
+        chart: 'Diagram',
       },
     },
     aria: {
