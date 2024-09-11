e56872ee199a03f64b402ff01d8eea552e9f4ba9
lmmelstron <93156969+lmmelstron@users.noreply.github.com>
2024-09-11 16:01:29 +0300

    fix(pie) label hover visibility

--- dist/echarts.simple.js
+++ dist/echarts.simple.js
@@ -41916,6 +41916,10 @@
         el.ignore = true;
       }
 
+      function setShow(el) {
+        el.ignore = false;
+      }
+
       function isLabelShown(label) {
         if (!label.ignore) {
           return true;
@@ -41948,17 +41952,13 @@
         labelLineLen = parsePercent$1(labelLineLen, viewWidth);
         var labelLineLen2 = labelLineModel.get('length2');
         labelLineLen2 = parsePercent$1(labelLineLen2, viewWidth);
+        var isLabelHidden = Math.abs(sectorShape.endAngle - sectorShape.startAngle) < minShowLabelRadian;
+        each(label.states, isLabelHidden ? setNotShow : setShow);
+        label.ignore = isLabelHidden;
 
-        if (Math.abs(sectorShape.endAngle - sectorShape.startAngle) < minShowLabelRadian) {
-          each(label.states, setNotShow);
-          label.ignore = true;
-
-          if (labelLine) {
-            each(labelLine.states, setNotShow);
-            labelLine.ignore = true;
-          }
-
-          return;
+        if (labelLine) {
+          each(labelLine.states, isLabelHidden ? setNotShow : setShow);
+          labelLine.ignore = isLabelHidden;
         }
 
         if (!isLabelShown(label)) {
