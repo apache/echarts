d8babbc640750f20c9ced1ebad603c20dc1cc860
linlang <2550244256@qq.com>
2024-11-11 08:34:21 +0800

    debug 20265

--- src/scale/helper.ts
+++ src/scale/helper.ts
@@ -127,11 +127,8 @@ export function contain(val: number, extent: [number, number]): boolean {
 }
 
 export function normalize(val: number, extent: [number, number]): number {
-    if (isNaN(val)) {
-        return NaN;
-    }
     if (extent[1] === extent[0]) {
-        return 0.5;
+        return isNaN(val) ? NaN : 0.5;
     }
     return (val - extent[0]) / (extent[1] - extent[0]);
 }
