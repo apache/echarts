3aac20d7fe5fa65d8c451a3e645283d4007b369e
Ovilia <zwl.sjtu@gmail.com>
2025-06-18 17:46:26 +0800

    fix(marker): fix bug since #20166

--- src/component/marker/markerHelper.ts
+++ src/component/marker/markerHelper.ts
@@ -154,8 +154,12 @@ export function dataTransform(
     if (item.coord == null || !isArray(dims)) {
         item.coord = [];
         const baseAxis = seriesModel.getBaseAxis();
-        const otherAxis = coordSys.getOtherAxis(baseAxis);
-        item.value = numCalculate(data, data.mapDimension(otherAxis.dim), item.type);
+        if (baseAxis) {
+            const otherAxis = coordSys.getOtherAxis(baseAxis);
+            if (otherAxis) {
+                item.value = numCalculate(data, data.mapDimension(otherAxis.dim), item.type);
+            }
+        }
     }
     else {
         // Each coord support max, min, average
